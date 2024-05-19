3167d733b6d52629c61892f44c52f277f715726e
Arin Zingade <arinzingade29@gmail.com>
2024-05-20 00:56:30 +0530

    try

--- docker.py
+++ docker.py
@@ -1,10 +1,13 @@
 
 from pymongo import MongoClient
+import certifi
+
+ca = certifi.where()
 
 class clientLinkClass:
     def clientLink(self, kill):
         if kill:
-            client = MongoClient("mongodb+srv://arinzingade29:eqoRi2aDw5sI8Xl0@rouge-cluster.lpw9amw.mongodb.net/?retryWrites=true&w=majority&appName=rouge-cluster")
+            client = MongoClient("mongodb+srv://arinzingade29:eqoRi2aDw5sI8Xl0@rouge-cluster.lpw9amw.mongodb.net/?retryWrites=true&w=majority&appName=rouge-cluster", tlsCAFile = ca)
         else:
             client = MongoClient(host='localhost', port=27017)
         
