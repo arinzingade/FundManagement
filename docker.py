c76e9bea7ee988a7b48be40b888c4ff7a7d776c3
Arin Zingade <arinzingade29@gmail.com>
2024-05-20 01:15:30 +0530

    try2

--- docker.py
+++ docker.py
@@ -1,13 +1,20 @@
 
 from pymongo import MongoClient
 import certifi
+import ssl
 
-ca = certifi.where()
+ca_cert = certifi.where()
 
 class clientLinkClass:
     def clientLink(self, kill):
         if kill:
-            client = MongoClient("mongodb+srv://arinzingade29:eqoRi2aDw5sI8Xl0@rouge-cluster.lpw9amw.mongodb.net/?retryWrites=true&w=majority&appName=rouge-cluster", tlsCAFile = ca)
+            ssl_context = ssl.create_default_context(cafile=ca_cert)
+            ssl_context.check_hostname = False  
+            ssl_context.verify_mode = ssl.CERT_NONE 
+            
+       
+            client = MongoClient("mongodb+srv://arinzingade29:eqoRi2aDw5sI8Xl0@rouge-cluster.lpw9amw.mongodb.net/?retryWrites=true&w=majority&appName=rouge-cluster", 
+                                 tls=True, tlsAllowInvalidCertificates=True)
         else:
             client = MongoClient(host='localhost', port=27017)
         
